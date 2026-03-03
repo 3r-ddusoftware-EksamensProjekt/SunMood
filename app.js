60aed4c5c0c7694460f791cf231ade7667c0bbe2
CupcakeFlip <70489875+CupcakeFlip@users.noreply.github.com>
2026-03-03 13:57:02 +0100

    fjerne data.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 5000;
-const data = require("./routes/data.js"); // Load js to insert and get data from db
 
 // express()
 //     .use(express.static(path.join(__dirname, "public")))        // Make files in public folder available
@@ -12,7 +11,6 @@ express()
   .set("view engine", "ejs")
   .set("views", path.join(__dirname, "views"))
   .use(express.static(path.join(__dirname, "public")))
-  .use("/data", data) // Use instructions in data.js at url: <your-app-name>.herokuapp.com/data
   .get("/", (req, res) => res.render("pages/startSide.ejs"))
   .get("/startSide", (req, res) => res.render("pages/startSide.ejs"))
   .get("/sunMood", (req, res) => res.render("pages/sunMood.ejs"))
